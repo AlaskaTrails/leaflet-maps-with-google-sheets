50f6aa4d0b7a5e8216f3ab86ef6d66b5ec35871d
AlaskaTrails <36656474+AlaskaTrails@users.noreply.github.com>
2018-02-19 21:53:37 -0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JQaX_97Xlzzy2st0tDA-c9MsEq0_pLO0Kck2zPRKbNI/edit#gid=0';
